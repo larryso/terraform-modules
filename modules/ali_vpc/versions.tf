522724fb321d25cf2c5713a4ed4ea2d6d0254a9f
songdlb <songdlb@dcits.com>
2024-07-17 19:20:35 +0800

    add ali vpc module

--- modules/ali_vpc/versions.tf
+++ modules/ali_vpc/versions.tf
@@ -3,7 +3,8 @@ terraform{
     required_providers {
       alicloud = {
         source = "aliyun/alicloud"
-        version = ">= 1.214.1"
+        # version = ">= 1.214.1"
+        version = "~> 1.188.0"
       }
     }
 }
